f8282f17a58cd8454a65212f43d9414bed3e1f69
redian23 <vasya.degski@gmail.com>
2019-10-24 20:41:42 +0300

    Upload o n GH

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List listUsers = new LinkedList();
+        listUsers.add("Red");
+        listUsers.add("West");
+        listUsers.add("Andry");
+        listUsers.add("Wotson");
+        System.out.println(listUsers);
+
+    }
+}
